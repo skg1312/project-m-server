0608857b48c6f88c19768e9ac91e4d6a1ec9d9af
Santosh Shet <santushet@gmail.com>
2023-12-07 12:33:54 +0530

    A4 page setup

--- invoicepdf/pdfController.js
+++ invoicepdf/pdfController.js
@@ -72,11 +72,9 @@ exports.generatePdf = async (info = { filename: 'pdf_file', format: 'A4' }, resu
         const html = result;
         let options = {
           // "height": "14.25in",
-          // "width": "11.25in",
+          "width": "11.25in",
           // "header": { "height": "0mm" },
           // "footer": { "height": "0mm" },
-          format: 'A4',
-          orientation: 'portrait',
           childProcessOptions: {
             env: {
               OPENSSL_CONF: '/dev/null',
